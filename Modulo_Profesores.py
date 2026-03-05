c95c1fef13bfe50692b07e50b8159ea402d404be
DavieL <davielgiovanny0607@gmail.com>
2026-03-04 20:56:06 -0400

    primera prueba git

---
+++ Modulo_Profesores.py
@@ -0,0 +1,9 @@
+class Profesores():
+    def __init__(self, nombre, cedula, correo, materias_perm, list_materias):
+        self.nombre = nombre
+        self.cedula = cedula
+        self.correo = correo
+        self.materias_perm = materias_perm
+        self.list_materias = list_materias
+
+    def 
